89e182a7ac9f4f70fa83f5705b2afcc45f30ab55
Gregor499 <postfach@gregor-dirks.de>
2022-06-22 11:04:41 +0200

    Greeting Controller hinzugefügt

---
+++ src/main/java/com/github/gregor499/HerokuTest/HerokuTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.gregor499.HerokuTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuTestApplication.class, args);
+	}
+
+}
